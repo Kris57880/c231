b45b9dd0390a5cd292376c0ddd348840f4a4b188
Kris57880 <45487107+Kris57880@users.noreply.github.com>
2018-12-13 13:09:34 +0800

    Update c231.cpp

--- c231/c231.cpp
+++ c231/c231.cpp
@@ -4,57 +4,64 @@
 #include "stdafx.h"
 #pragma warning (default:4996)
 
+//231 
+#include<iostream>
 #include<cstdio>
-#include<algorithm>
-#include<deque>
+#include<utility>
 #include<list>
-#include<vector>
+#include<deque> 
+#include<algorithm>
 using namespace std;
-struct _node {
-	int x;
-	int y;
-} typedef Node;
-bool cmp(Node& a, Node& b) {
-	if (a.x == b.x) return a.y < b.y;
-	else return a.x < b.x;
+int cmp(pair<int,int>&a,pair<int,int>&b){
+	if(a.first==b.first)	return a.second>b.second;
+	else	return a.first>b.first;
 }
-bool rel(Node a, Node b) {
-	if (((a.x - b.x) > -3) && ((a.x - b.x) < 3) && ((a.y - b.y) > -3) && ((a.y - b.y) < 3)) return true;
-	else return false;
+int rel(pair<int,int> tmp,pair<int,int> bomb){
+	if(tmp.first-bomb.first<=2&&tmp.first-bomb.first>=-2){
+		if(tmp.second-bomb.second<=2&&tmp.second-bomb.second>=-2)
+			return true	;
+	}else	return false;
 }
-
-list<Node> mine;
-deque<Node> qu;
-int main() {
-	int r, c, num, count, now, next;
-	Node tmp;
-	while (scanf("%d", &r) != EOF) {
-		scanf("%d %d", &c, &num);
-		count = 0;
-		for (int i = 0; i<num; i++) {
-			scanf("%d %d", &(tmp.x), &(tmp.y));
-			mine.push_back(tmp);
+ int main()
+{
+	list< pair<int,int> > Bomb_Address;
+	int a,b,c;
+	while(/*scanf("%d,%d,%d",&a,&b,&c)!=EOF*/cin>>a>>b>>c){
+		int i,x,y;
+		for(i=1;i<=c;i++){
+			cin>>x>>y;
+			//scanf("%d,%d",&x,&y);
+			pair<int,int> tmp(x,y);
+			Bomb_Address.push_back(tmp);
 		}
-		mine.sort(cmp);         //地雷依照x由小到大，再依照y由小到大
-		while (mine.size() > 0) {
-			qu.push_back(mine.front());  //從mine取出放到qu，qu表示目前可以引爆的地雷
-			mine.pop_front();   //mine刪除剛剛取出放到qu的地雷，避免重複計算
-			while (qu.size() > 0) {    //可以連鎖反應的地雷放到qu
-				tmp = qu.front();   //取出從qu取出第一個放到tmp
-				qu.pop_front();    //qu刪除第一個元素
-				for (list<Node>::iterator it = mine.begin(); it != mine.end();) {  //迴圈中刪除it++，由第43與45行處理
-					if (((*it).x - tmp.x) > 2) break;   //因為mine依照x由小到大排序，x座標差超過2就中斷迴圈
-					if (rel(tmp, *it)) {  //判斷tmp與*it是否是連鎖地雷
-						qu.push_back(*it);   //若是連鎖地雷加入qu
-						it = mine.erase(it);   //從mine刪除it，會自動回傳刪除元素的下一個元素位址到it，所以不用it++
+		Bomb_Address.sort(cmp);
+		/*
+		for(list<pair<int,int> >::iterator it=Bomb_Address.begin();it!=Bomb_Address.end();it++)
+			cout<<it->first<<it->second;
+		*/
+		int used=0;
+		while(Bomb_Address.empty()==false){
+			deque<pair<int,int> >BombChain;
+			BombChain.push_front(Bomb_Address.front() );
+			Bomb_Address.pop_front(); 
+			while(BombChain.size()>0){
+				pair<int,int>tmp=BombChain.front();
+				BombChain.pop_front();
+				for	(list<pair<int,int> >::iterator it=Bomb_Address.begin();it!=Bomb_Address.end();){
+					if((*it).first-tmp.first>2&&(*it).second-tmp.second>2) break;
+					if(rel(tmp,*it)){
+						BombChain.push_back(*it);
+						Bomb_Address.erase(it);
 					}
-					else {
-						it++;     //找下一個
-					}
-				}
+					else 
+						it++;
+				}	
 			}
-			count++;
+			used++;
 		}
-		printf("%d\n", count);
-	}
+		//printf("%d\n",used);
+		cout<<used<<"\n";
+		Bomb_Address.clear();
+		   }
+   return 0;
 }
